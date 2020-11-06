be86704b656b791d4608fff1d3744d3c540ff6d3
Erick Araujo <erickaraujo2514@gmail.com>
2020-11-06 09:20:22 -0300

    Criando repositorio com exercicios de csharp da Alura

---
+++ 06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,57 @@
+﻿// using _06_ByteBank;
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        private double saldo = 100;
+
+
+        public void setSaldo (double saldo)
+        {
+            if(saldo < 0)
+            {
+                return;
+            }
+  
+               this.saldo = saldo;
+          
+        }
+        public double GetSaldo()
+        {
+            return saldo;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
